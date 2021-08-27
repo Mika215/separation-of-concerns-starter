206ab4ba32e140bcfb4f65debc686b30b034faf7
reinaldo <perezrei@gmail.com>
2021-08-27 12:18:49 +0200

    eliminate li element

--- client/src/init/index.js
+++ client/src/init/index.js
@@ -1,4 +1,15 @@
+// variables
 (function(){
-    const 
+    const activityList = document.getElementById('ordered-list');
+    const activityInput = document.getElementById('todo-input');
+    const addButton = document.getElementById('todo-button');
+    // functions
+    const addActivity = function(){
 
-}())
+
+    }
+
+    // events
+    addButton.addEventListener('click', addActivity);
+}());
+
